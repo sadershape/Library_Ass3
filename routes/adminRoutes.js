28e07a9b9e6caf8deb4b7856545a4ade5618c558
sadershape <231564@astanait.edu.kz>
2025-01-27 01:18:35 +0500

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User"); // Assuming you have a User model
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { isAdmin } = require("../middleware/authMiddleware"); // Admin check middleware
 
 // Admin Dashboard
