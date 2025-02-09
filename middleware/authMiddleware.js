fe2ba35a88d7b07aaa10e3e86b9a1fe637c17a32
sadershape <231564@astanait.edu.kz>
2025-02-09 15:00:42 +0500

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 module.exports.isAdmin = (req, res, next) => {
-    if (req.session.user && req.session.user.isAdmin) {
-        next();
-    } else {
-        res.status(403).send("Access Denied");
+    if (!req.session.user || req.session.user.role !== "admin") {
+        return res.status(403).send("❌ Access Denied: Admins Only.");
     }
+    next();
 };
