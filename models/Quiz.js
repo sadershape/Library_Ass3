6e5338f917da6db32c12e27d3d1e9c55f6ba2bfb
sadershape <231564@astanait.edu.kz>
2025-02-26 13:35:26 +0500

    Update Quiz.js

--- models/Quiz.js
+++ models/Quiz.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const quizSchema = new mongoose.Schema({
     question: { type: String, required: true },
     options: [{ type: String, required: true }],
-    correctAnswer: { type: String, required: true },
+    answer: { type: String, required: true },
+    correctAnswer: { type: String, required: true } // Add correctAnswer field
 });
 
 module.exports = mongoose.model("Quiz", quizSchema);
