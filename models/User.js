1d177de385667a8fe3f410c6342979a1212dded5
sadershape <231564@astanait.edu.kz>
2025-02-26 13:30:18 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
-
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: "user" },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
+    quizResults: [{
+        score: Number,
+        date: { type: Date, default: Date.now }
+    }]
 });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
-
