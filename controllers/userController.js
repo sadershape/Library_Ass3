b76ba3ad58e40ea6572d2af6a48431ffadcfba98
sadershape <231564@astanait.edu.kz>
2025-02-26 13:53:20 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,12 @@ const User = require("../models/User");
 exports.getUserProfile = async (req, res) => {
     try {
         const user = await User.findById(req.session.user._id).lean();
+        if (!user) {
+            return res.status(404).render("userProfile", { user: null, error: "User not found." });
+        }
         res.render("userProfile", { user });
     } catch (error) {
         console.error("Error fetching user profile:", error);
-        res.render("userProfile", { user: null });
+        res.render("userProfile", { user: null, error: "An error occurred while fetching the user profile." });
     }
 };
