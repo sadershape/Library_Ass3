b9105fb587f27aa2b1bdeb413ccc84df8b77184b
sadershape <231564@astanait.edu.kz>
2025-02-26 14:07:38 +0500

    Update QuizResult.js

--- models/QuizResult.js
+++ models/QuizResult.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const quizResultSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     score: { type: Number, required: true },
     totalQuestions: { type: Number, required: true },
     completedInTime: { type: Boolean, required: true },
-    timestamp: { type: Date, default: Date.now },
+    date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("QuizResult", quizResultSchema);
